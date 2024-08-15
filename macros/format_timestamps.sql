6dd5cd333fe78f6bb9fa79fb85be2cd9cd6d5635
FilipeTheAnalyst <fbalseiro@gmail.com>
2024-08-15 01:52:14 +0100

    added source and stg model for atp_matches

--- macros/format_timestamps.sql
+++ macros/format_timestamps.sql
@@ -1,41 +1,41 @@
 -- macro that formats a timestamp to a date key that references the DIM_DATE dimension table
 {% macro to_date_key(timestamp_column) %}
-    {% set _result = "strftime(" ~ timestamp_column ~ ", '%Y%m%d')" %}
+    {% set _result = "TO_CHAR(" ~ timestamp_column ~ ", 'YYYYMMDD')" %}
 
     {{ return(_result) }}
 {% endmacro %}
 
 -- macro that formats a timestamp to a time key that references the DIM_TIME dimension table
 {% macro to_time_key(timestamp_column) %}
-    {% set _result = "strftime(" ~ timestamp_column ~ ", '%H%M')" %}
+    {% set _result = "TO_CHAR(" ~ timestamp_column ~ ", '%H%M')" %}
 
     {{ return(_result) }}
 {% endmacro %}
 
 -- macro that formats a timestamp to the ISO8601 date format
 {% macro to_iso_date(timestamp_column) %}
-    {% set _result = "strftime(" ~ timestamp_column ~ ", '%Y-%m-%d')" %}
+    {% set _result = "TO_CHAR(" ~ timestamp_column ~ ", '%Y-%m-%d')" %}
 
     {{ return(_result) }}
 {% endmacro %}
 
 -- macro that formats a timestamp to the ISO8601 date format but with dots delimiters instead of dashes
 {% macro to_iso_date_us(timestamp_column) %}
-    {% set _result = "strftime(" ~ timestamp_column ~ ", '%Y.%m.%d')" %}
+    {% set _result = "TO_CHAR(" ~ timestamp_column ~ ", '%Y.%m.%d')" %}
 
     {{ return(_result) }}
 {% endmacro %}
 
 -- macro that formats a timestamp to British date
 {% macro to_date_gb(timestamp_column) %}
-    {% set _result = "strftime(" ~ timestamp_column ~ ", '%d/%m/%Y')" %}
+    {% set _result = "TO_CHAR(" ~ timestamp_column ~ ", '%d/%m/%Y')" %}
 
     {{ return(_result) }}
 {% endmacro %}
 
 -- macro that formats a timestamp to US date
 {% macro to_date_us(timestamp_column) %}
-    {% set _result = "strftime(" ~ timestamp_column ~ ", '%m/%d/%Y')" %}
+    {% set _result = "TO_CHAR(" ~ timestamp_column ~ ", '%m/%d/%Y')" %}
 
     {{ return(_result) }}
 {% endmacro %}
@@ -45,17 +45,4 @@
     {% set _result = "(year(current_date) - year(" ~ timestamp_column ~ "))" %}
 
     {{ return(_result) }}
-{% endmacro %}
-
--- macro to convert date in integer data type to date
-{% macro convert_yyyymmdd_int_to_date(yyyymmdd_int) %}
-    {{ 
-        return(
-            "MAKE_DATE("
-            ~ "CAST(SUBSTR(CAST(" ~ yyyymmdd_int ~ " AS VARCHAR), 1, 4) AS INT), "
-            ~ "CAST(SUBSTR(CAST(" ~ yyyymmdd_int ~ " AS VARCHAR), 5, 2) AS INT), "
-            ~ "CAST(SUBSTR(CAST(" ~ yyyymmdd_int ~ " AS VARCHAR), 7, 2) AS INT)"
-            ~ ")"
-        ) 
-    }}
 {% endmacro %}
--- models/1_stage/stg_atp__matches.sql
+++ models/1_stage/stg_atp__matches.sql
@@ -1,26 +1,25 @@
 WITH source AS (
     SELECT *
-    FROM {{ source('atp_tour_raw', 'matches') }}
-),
-
-renamed AS (
+    FROM {{ source('atp_tour_raw', 'atp_matches') }}
+)
+, renamed AS (
     SELECT
-        tourney_id::varchar(50) AS tournament_id,
-        tourney_name::varchar(100) AS tournament_name,
+        tourney_id::VARCHAR(50) AS tournament_id,
+        tourney_name::VARCHAR(100) AS tournament_name,
         CASE
             WHEN tourney_level = 'A' THEN 'Other tour-level events'
             WHEN tourney_level = 'D' THEN 'Davis Cup'
             WHEN tourney_level = 'F' THEN 'Tour finals'
             WHEN tourney_level = 'G' THEN 'Grand Slams'
             WHEN tourney_level = 'M' THEN 'Masters 1000s'
-        END::varchar(25) AS tournament_level,
-        {{ convert_yyyymmdd_int_to_date('tourney_date') }} AS tournament_date,
-        surface::varchar(10) AS surface,
-        draw_size::smallint AS draw_size,
-        match_num::smallint AS match_id,
-        score::varchar(50) AS score,
-        best_of::tinyint AS best_of,
-        ('Best of ' || best_of)::varchar(10) AS best_of_labeled,
+        END::VARCHAR(25) AS tournament_level,
+        TO_DATE(TO_VARCHAR(tourney_date),'YYYYMMDD') AS tournament_date,
+        surface::VARCHAR(10) AS surface,
+        draw_size::SMALLINT AS draw_size,
+        match_num::SMALLINT AS match_id,
+        score::VARCHAR(50) AS score,
+        best_of::TINYINT AS best_of,
+        ('Best of ' || best_of)::VARCHAR(10) AS best_of_labeled,
         CASE
             WHEN round = 'BR' THEN ''
             WHEN round = 'ER' THEN ''
@@ -33,10 +32,10 @@ renamed AS (
             WHEN round = 'RR' THEN 'Round robin'
             WHEN round = 'SF' THEN 'Semifinal'
             ELSE round
-        END::varchar(4) AS round,
-        minutes::smallint AS minutes,
-        winner_id::int AS winner_id,
-        winner_seed::tinyint AS winner_seed,
+        END::VARCHAR(20) AS round,
+        minutes::SMALLINT AS minutes,
+        winner_id::INT AS winner_id,
+        winner_seed::TINYINT AS winner_seed,
         CASE
             WHEN winner_entry = 'WC' THEN 'Wild card'
             WHEN winner_entry = 'Q' THEN 'Qualifier'
@@ -44,31 +43,31 @@ renamed AS (
             WHEN winner_entry = 'PR' THEN 'Protected ranking'
             WHEN winner_entry = 'ITF' THEN 'ITF entry'
             ELSE winner_entry
-        END::varchar(20) AS winner_entry,
-        winner_name::varchar(100) AS winner_name,
+        END::VARCHAR(20) AS winner_entry,
+        winner_name::VARCHAR(100) AS winner_name,
         CASE
             WHEN winner_hand = 'R' THEN 'Right-handed'
             WHEN winner_hand = 'L' THEN 'Left-handed'
             WHEN winner_hand = 'A' THEN 'Ambidextrous'
             WHEN winner_hand = 'U' THEN 'Unknown'
             ELSE winner_hand
-        END::varchar(15) AS winner_dominant_hand,
-        winner_ht::smallint AS winner_height_cm,
-        winner_ioc::varchar(3) AS winner_country_iso_code,
-        winner_age::tinyint AS winner_age,
-        w_ace::tinyint AS winner_num_of_aces,
-        w_df::smallint AS winner_num_of_double_faults,
-        w_svpt::smallint AS winner_num_of_serve_pts,
-        w_1st_in::smallint AS winner_num_of_1st_serves_made,
-        w_1st_won::smallint AS winner_num_of_1st_serve_pts_won,
-        w_2nd_won::smallint AS winner_num_of_2nd_serve_pts_won,
-        w_sv_gms::smallint AS winner_num_of_serve_games,
-        w_bp_saved::smallint AS winner_num_of_break_pts_saved,
-        w_bp_faced::smallint AS winner_num_of_break_pts_faced,
-        winner_rank::smallint AS winner_rank,
-        winner_rank_points::smallint AS winner_rank_pts,
-        loser_id::int AS loser_id,
-        loser_seed::tinyint AS loser_seed,
+        END::VARCHAR(15) AS winner_dominant_hand,
+        winner_ht::SMALLINT AS winner_height_cm,
+        winner_ioc::VARCHAR(3) AS winner_country_iso_code,
+        winner_age::TINYINT AS winner_age,
+        w_ace::TINYINT AS winner_num_of_aces,
+        w_df::SMALLINT AS winner_num_of_double_faults,
+        w_svpt::SMALLINT AS winner_num_of_serve_pts,
+        w_1stin::SMALLINT AS winner_num_of_1st_serves_made,
+        w_1stwon::SMALLINT AS winner_num_of_1st_serve_pts_won,
+        w_2ndwon::SMALLINT AS winner_num_of_2nd_serve_pts_won,
+        w_svgms::SMALLINT AS winner_num_of_serve_games,
+        w_bpsaved::SMALLINT AS winner_num_of_break_pts_saved,
+        w_bpfaced::SMALLINT AS winner_num_of_break_pts_faced,
+        winner_rank::SMALLINT AS winner_rank,
+        winner_rank_points::SMALLINT AS winner_rank_pts,
+        loser_id::INT AS loser_id,
+        loser_seed::TINYINT AS loser_seed,
         CASE
             WHEN loser_entry = 'WC' THEN 'Wild card'
             WHEN loser_entry = 'Q' THEN 'Qualifier'
@@ -76,57 +75,52 @@ renamed AS (
             WHEN loser_entry = 'PR' THEN 'Protected ranking'
             WHEN loser_entry = 'ITF' THEN 'ITF entry'
             ELSE loser_entry
-        END::varchar(20) AS loser_entry,
-        loser_name::varchar(100) AS loser_name,
+        END::VARCHAR(20) AS loser_entry,
+        loser_name::VARCHAR(100) AS loser_name,
         CASE
             WHEN loser_hand = 'R' THEN 'Right-handed'
             WHEN loser_hand = 'L' THEN 'Left-handed'
             WHEN loser_hand = 'A' THEN 'Ambidextrous'
             WHEN loser_hand = 'U' THEN 'Unknown'
             ELSE loser_hand
-        END::varchar(15) AS loser_dominant_hand,
-        loser_ht::smallint AS loser_height_cm,
-        loser_ioc::varchar(3) AS loser_country_iso_code,
-        loser_age::tinyint AS loser_age,
-        l_ace::tinyint AS loser_num_of_aces,
-        l_df::smallint AS loser_num_of_double_faults,
-        l_svpt::smallint AS loser_num_of_serve_pts,
-        l_1st_in::smallint AS loser_num_of_1st_serves_made,
-        l_1st_won::smallint AS loser_num_of_1st_serve_pts_won,
-        l_2nd_won::smallint AS loser_num_of_2nd_serve_pts_won,
-        l_sv_gms::smallint AS loser_num_of_serve_games,
-        l_bp_saved::smallint AS loser_num_of_break_pts_saved,
-        l_bp_faced::smallint AS loser_num_of_break_pts_faced,
-        loser_rank::smallint AS loser_rank,
-        loser_rank_points::smallint AS loser_rank_pts,
-        w_ace::int + l_ace::int AS total_num_of_aces,
-        w_df::int + l_df::int AS total_num_of_double_faults,
-        w_svpt::int + l_svpt::int AS total_num_of_serve_pts,
-        w_1st_in::int + l_1st_in::int AS total_num_of_1st_serves_made,
-        w_1st_won::int + l_1st_won::int AS total_num_of_1st_serve_pts_won,
-        w_2nd_won::int + l_2nd_won::int AS total_num_of_2nd_serve_pts_won,
-        w_sv_gms::int + l_sv_gms::int AS total_num_of_serve_games,
-        w_bp_saved::int + l_bp_saved::int AS total_num_of_break_pts_saved,
-        w_bp_faced::int + l_bp_faced::int AS total_num_of_break_pts_faced,
-        abs(winner_age::tinyint - loser_age::tinyint) AS age_difference
+        END::VARCHAR(15) AS loser_dominant_hand,
+        loser_ht::SMALLINT AS loser_height_cm,
+        loser_ioc::VARCHAR(3) AS loser_country_iso_code,
+        loser_age::TINYINT AS loser_age,
+        l_ace::TINYINT AS loser_num_of_aces,
+        l_df::SMALLINT AS loser_num_of_double_faults,
+        l_svpt::SMALLINT AS loser_num_of_serve_pts,
+        l_1stin::SMALLINT AS loser_num_of_1st_serves_made,
+        l_1stwon::SMALLINT AS loser_num_of_1st_serve_pts_won,
+        l_2ndwon::SMALLINT AS loser_num_of_2nd_serve_pts_won,
+        l_svgms::SMALLINT AS loser_num_of_serve_games,
+        l_bpsaved::SMALLINT AS loser_num_of_break_pts_saved,
+        l_bpfaced::SMALLINT AS loser_num_of_break_pts_faced,
+        loser_rank::SMALLINT AS loser_rank,
+        loser_rank_points::SMALLINT AS loser_rank_pts,
+        w_ace::INT + l_ace::INT AS total_num_of_aces,
+        w_df::INT + l_df::INT AS total_num_of_double_faults,
+        w_svpt::INT + l_svpt::INT AS total_num_of_serve_pts,
+        w_1stin::INT + l_1stin::INT AS total_num_of_1st_serves_made,
+        w_1stwon::INT + l_1stwon::INT AS total_num_of_1st_serve_pts_won,
+        w_2ndwon::INT + l_2ndwon::INT AS total_num_of_2nd_serve_pts_won,
+        w_svgms::INT + l_svgms::INT AS total_num_of_serve_games,
+        w_bpsaved::INT + l_bpsaved::INT AS total_num_of_break_pts_saved,
+        w_bpfaced::INT + l_bpfaced::INT AS total_num_of_break_pts_faced,
+        abs(winner_age::TINYINT - loser_age::TINYINT) AS age_difference
     FROM source
-),
-
-final AS (
+)
+, final AS (
     SELECT
 
-
         {{ dbt_utils.generate_surrogate_key(['tournament_id', 'tournament_date']) }}
-            AS tournament_sk
-        ,
-
+            AS tournament_sk,
         {{ dbt_utils.generate_surrogate_key(['tournament_id', 'match_id']) }} AS match_sk,
-        {{ to_date_key('tournament_date') }}::int AS tournament_date_key,
+        {{ to_date_key('tournament_date') }}::INT AS tournament_date_key,
         {{ dbt_utils.generate_surrogate_key(['winner_id']) }} AS player_winner_key,
         {{ dbt_utils.generate_surrogate_key(['loser_id']) }} AS player_loser_key,
         *
     FROM renamed
 )
-
 SELECT *
 FROM final
